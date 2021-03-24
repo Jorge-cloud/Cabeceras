b9ce95eb8962bad0bb88468738cb55bbdd70c70e
Jorge <Jorge@DESKTOP-EI54MJL>
2021-03-23 20:08:18 -0400

    primerComit

---
+++ src/java/com/emergentes/ResoJSON.java
@@ -0,0 +1,40 @@
+
+package com.emergentes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Jorge
+ */
+@WebServlet(name = "ResoJSON", urlPatterns = {"/ResoJSON"})
+public class ResoJSON extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setHeader("Content-Disposition", "attachment;filename=empleados.json");
+        PrintWriter out = response.getWriter();
+        try{
+            out.println("{'empleados':[");
+            out.println("{'nombre':'Bruno','apellidos':'Diaz'},");
+            out.println("{'nombre':'Juan Jose','apellidos':'Paredes'},");
+            out.println("{'nombre':'Dolores','apellidos':'Fuentes'}");
+            out.println("}]");
+            
+            
+        }finally{
+            out.close();
+        }
+    }
+
+    
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : index
+    Created on : 23-mar-2021, 19:44:38
+    Author     : Jorge
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Respuesta usando cabeceras en un Servlet</h1>
+        <ul>
+            <li><a href="RespHTML">Developer un HTML</a></li>
+            <li><a href="RespXLS">Developer un XLS</a></li>
+            <li><a href="RespXML">Developer un XML</a></li>
+            <li><a href="ResoJSON">Developer un JSON</a></li>
+            <li><a href="RespCSV">Developer un CSV</a></li>
+            <li><a href="RespIMG">Developer una Imagen</a></li>
+        </ul>
+    </body>
+</html>
